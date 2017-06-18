9371bb0f8286d5156e0a411bed12768cee6393ee
Mark Berry <mark@MacBook-Pro.lan>
2017-06-18 11:37:38 -0500

    Add create-react-app-with-redux

--- src/components/ClientForm.js
+++ src/components/ClientForm.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 export default class ClientForm extends React.Component {
+
+  handleSubmit() {
+    console.log("Hello Handle Submit");
+  }
+
   render() {
     return <div>
       <span>New Client Form</span>
       <form>
         <label>Name</label>
         <input/>
-        <button>Submit</button>
+        <button onClick={this.handleSubmit()}>Submit</button>
       </form> 
     </div>
   }
